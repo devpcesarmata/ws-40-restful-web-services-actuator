9d3d3eb9ba153c07da5d595a8f3d2e50fa808357
César <dev.pcesarmata@gmail.com>
2020-09-10 18:00:48 -0500

    init commit.

---
+++ src/main/java/com/cesarmata/ws40restfulwebservicesactuator/Ws40RestfulWebServicesActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws40restfulwebservicesactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws40RestfulWebServicesActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws40RestfulWebServicesActuatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesarmata/ws40restfulwebservicesactuator/Ws40RestfulWebServicesActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws40restfulwebservicesactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws40RestfulWebServicesActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
