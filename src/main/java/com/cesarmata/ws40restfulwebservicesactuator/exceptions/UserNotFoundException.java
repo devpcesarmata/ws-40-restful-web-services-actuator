78cda3bfefbd6e603b4deb913f6dd0dff17dbd86
César <dev.pcesarmata@gmail.com>
2020-09-10 19:52:41 -0500

    Se concluye con la clase 40 (actuators).

---
+++ src/main/java/com/cesarmata/ws40restfulwebservicesactuator/exceptions/UserNotFoundException.java
@@ -0,0 +1,17 @@
+package com.cesarmata.ws40restfulwebservicesactuator.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author Cesar Mata.
+ */
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+	
+	public UserNotFoundException(String message){
+		super(message);
+	}
+	
+}
